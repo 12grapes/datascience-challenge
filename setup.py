c5608794669fc12d563a41b2d4bbb65c0b3f7ede
andyrawasy@hotmail.com <andyrawasy@hotmail.com>
2018-04-03 23:43:22 +0100

    Adding models folder and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="datascience-challenge",
+    version="1.0",
+    description="datascience-challenge",
+    url="https://github.com/franciscomcdias/datascience-challenge",
+    author="Francisco Dias",
+    author_email="mail@franciscodias.pt",
+    license="MIT",
+    packages=[],
+    zip_safe=False
+)
